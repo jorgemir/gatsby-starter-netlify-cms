2fc183d328941fc37d9213d794daa2c9d3106746
Jorge Mir <jorgemir@gmail.com>
2019-08-24 00:14:56 -0700

    believe it

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,12 +3,16 @@ import Helmet from 'react-helmet'
 
 import Navbar from '../components/Navbar'
 import './all.sass'
+import waves from '../img/waves.svg'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Oh Yeahhhh" />
     <Navbar />
     <div>{children}</div>
+    <span className="waves">
+      <img src={waves} alt="Waves" />
+    </span>
   </div>
 )
 
